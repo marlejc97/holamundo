bbb5591e6f20263370dfc004756605739613b940
marleeee <147526640+marlejc97@users.noreply.github.com>
2023-10-31 10:09:56 -0500

    Add files via upload

---
+++ holamundo/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BIENVENIDO</h1>
+    
+    <h1><img src="img/JK.png" alt="" width=300px ></h1>
+    <?php
+    echo "HOLA MARLE";
+    ?>
+</body>
+</html>
+
+
+
+
+
+<h1></h1>
+<?php
+echo "HOLA MUDO";
+
+?>
+<label> Ingres nombre</label>
+<input type="text" name="txtNom">
+<label><?php echo "JK";?></label>
+
+
